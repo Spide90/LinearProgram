747df8b47edd0b1fc47d182cbb3fa5bbd35e4adf
chris <c.goldstein@goldstein-systemberatung.de>
2014-05-05 12:48:05 +0200

    testfile created, main method

--- src/io/Console.java
+++ src/io/Console.java
@@ -1,10 +1,12 @@
 package io;
 
+import java.io.File;
+
 public class Console {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		File file = new File("testFile.lp");
+		new LPReader(file);
 	}
 
 }
